f6315483303c7f6a16edb0cdb00498209114f3f8
WLLKAE001 <wllkae001@uct.ac.za>
2019-08-11 22:52:26 +0200

    Sign change on fertilty calculation

--- Terrain.py
+++ Terrain.py
@@ -36,7 +36,7 @@ class Terrain:
 	def setFertility(self, beta, alpha, mu):
 		self.harvested = False
 		self.years_not_harvested += 1
-		self.fertility = 17*(beta*(np.exp((mu - self.nile_distance)**2 / alpha)))
+		self.fertility = 17*(beta*(np.exp(-(mu - self.nile_distance)**2 / alpha)))
 		if self.owner != None:
 			self.harvest = self.fertility*self.max_yield*self.owner.competency - self.house_distance*self.owner.distance_cost
 
